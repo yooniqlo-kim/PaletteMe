52fe318b2db47693400c2ea78e37b6970faa5a9b
yoon <yoon733@naver.com>
2025-03-24 10:58:06 +0900

    ✨[BE] feat: 공통 응답 클래스 구현 (S12P21A505-144)

---
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/common/response/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.ssafy.paletteme.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ApiResponse<T> {
+    private boolean success;
+    private String errorMsg;
+    private String errorCode;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true,null,null,data);
+    }
+
+    public static <T> ApiResponse<T> error(String errorCode, String errorMsg) {
+        return new ApiResponse<>(false, errorMsg, errorCode, null);
+    }
+
+}
