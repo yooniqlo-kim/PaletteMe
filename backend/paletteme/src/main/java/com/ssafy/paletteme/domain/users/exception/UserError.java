c4d5267d4c90e0d20c11dea35421c4a0c34cda3b
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-03-25 13:47:51 +0900

    ✨ [BE] feat: 회원가입 예외처리 추가(S12P21A505-49)

--- backend/paletteme/src/main/java/com/ssafy/paletteme/domain/users/exception/UserError.java
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/domain/users/exception/UserError.java
@@ -6,7 +6,12 @@ import lombok.Getter;
 public enum UserError {
 
     USER_NOT_FOUND("400", "1001", "가입되지 않은 사용자입니다."),
+    DUPLICATE_LOGIN_ID("400", "1002", "이미 존재하는 아이디입니다."),
+    SIGNUP_USERS_GRADE("500", "1003", "회원가입에 실패하였습니다."),
+    SIGNUP_USERS_COLOR("500", "1004", "회원가입에 실패하였습니다."),
+    SIGNUP_USERS_IMAGE_UPLOAD("500", "1005", "회원가입에 실패하였습니다.")
     ;
+
 
     private final String httpStatusCode;
     private final String errorCode;
