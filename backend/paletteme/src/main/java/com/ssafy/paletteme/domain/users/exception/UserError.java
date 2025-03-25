7c5ebe9cdd563d64e1565397370efa2dee88756c
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-03-25 14:06:46 +0900

    ♻️ [BE] refact: 에러메세지 변경(S12P21A505-49)

--- backend/paletteme/src/main/java/com/ssafy/paletteme/domain/users/exception/UserError.java
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/domain/users/exception/UserError.java
@@ -7,9 +7,9 @@ public enum UserError {
 
     USER_NOT_FOUND("400", "1001", "가입되지 않은 사용자입니다."),
     DUPLICATE_LOGIN_ID("400", "1002", "이미 존재하는 아이디입니다."),
-    SIGNUP_USERS_GRADE("500", "1003", "회원가입에 실패하였습니다."),
-    SIGNUP_USERS_COLOR("500", "1004", "회원가입에 실패하였습니다."),
-    SIGNUP_USERS_IMAGE_UPLOAD("500", "1005", "회원가입에 실패하였습니다.")
+    SIGNUP_USERS_GRADE("500", "1003", "회원가입 시 등급 저장할때 오류!"),
+    SIGNUP_USERS_COLOR("500", "1004", "있는 색깔만 보내주라~"),
+    SIGNUP_USERS_IMAGE_UPLOAD("500", "1005", "s3 이미지 저장 오류!")
     ;
 
 
