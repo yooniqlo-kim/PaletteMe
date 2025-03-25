a6da7c122e7cd58ee48de99fa9bb6a3854d37c87
김지윤 <jaegal0920@gmail.com>
2025-03-25 14:32:39 +0900

    ✨ [FE] feat: icons 추가 (S12P21A505-142)

---
+++ frontend/src/shared/components/icons/IconLeftArrow.tsx
@@ -0,0 +1,23 @@
+export default function IconLeftArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="42"
+      viewBox="0 0 24 42"
+      fill="none">
+      <path
+        d="M21 3L3 21"
+        stroke="#B0B0B0"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M21.2322 39.2324L3.23218 21.2324"
+        stroke="#B0B0B0"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
---
+++ frontend/src/shared/components/icons/IconRedHeart.tsx
@@ -0,0 +1,24 @@
+// 작품 리스트에 보이는 빨간색 하트
+export default function IconRedHeart({ isClicked }: { isClicked: boolean }) {
+  let color = isClicked
+    ? "var(--color-navbar-active)"
+    : "var(--color-netural-3)";
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="27"
+      height="28"
+      viewBox="0 0 27 28"
+      fill="none">
+      <path
+        d="M7.98022 3.81885C4.97318 3.81885 2.53467 6.36686 2.53467 9.51049C2.53467 12.0482 3.48764 18.0709 12.8681 24.1593C13.0362 24.2672 13.2291 24.3243 13.4258 24.3243C13.6225 24.3243 13.8154 24.2672 13.9834 24.1593C23.3639 18.0709 24.3169 12.0482 24.3169 9.51049C24.3169 6.36686 21.8783 3.81885 18.8713 3.81885C15.8643 3.81885 13.4258 7.26833 13.4258 7.26833C13.4258 7.26833 10.9872 3.81885 7.98022 3.81885Z"
+        fill={color}
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
