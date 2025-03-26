b4b11c084cfaa64a588ab88ce92ea011521b6a13
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-03-26 16:27:09 +0900

    💡 [BE] comment: swagger 설명 추가(S12P21A505-49)

--- backend/paletteme/src/main/java/com/ssafy/paletteme/domain/users/controller/UserController.java
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/domain/users/controller/UserController.java
@@ -4,7 +4,9 @@ import com.ssafy.paletteme.common.response.ApiResponse;
 import com.ssafy.paletteme.common.security.annotation.UserId;
 import com.ssafy.paletteme.domain.users.dto.UserSignupRequest;
 import com.ssafy.paletteme.domain.users.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,18 +14,21 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Tag(name = "Users", description = "회원 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "폼데이터 형식으로 회원 정보를 전송, User의 개인 정보를 담은 데이터와 이미지를 담은 데이터를 분리해서 보내줘야합니다.(스웨거 테스트 불가)")
     @PostMapping(value = "/sign-up", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponse<Void> signUp(@RequestPart("data") UserSignupRequest request, @RequestPart(value = "file", required = false)  MultipartFile file) throws IOException {
         userService.signUp(request, file);
         return ApiResponse.success();
     }
 
+    @Operation(summary = "토큰 유효성 확인", description = "Access Token이 유효한지 확인하는 테스트용 API입니다.")
     @GetMapping(value = "/token-test")
     public ApiResponse<Integer> tokenTest(@Parameter(hidden = true) @UserId int userId){
         return ApiResponse.success(userId);
