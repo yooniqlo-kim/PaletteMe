3402ecbf7d56ebab57a871219e369493deed61fd
hyunhee <hh.hyunheekim@gmail.com>
2025-03-26 15:04:34 +0900

    ✨ [FE] feat: 감상문 티켓 컴포넌트 추가(S12P21A505-130)

--- frontend/src/pages/TodayArtsPage.tsx
+++ frontend/src/pages/TodayArtsPage.tsx
@@ -1,3 +1,21 @@
+import { CommentTicket } from "@/shared/components/comments/CommentTicket";
+
 export default function TodayArtsPage() {
-  return <div>TodayArtsPage</div>;
+  return (
+    <>
+      <div>TodayArtsPage</div>
+      <div className="flex justify-center">
+        <CommentTicket
+          artworkImageUrl="https://cdn.safetimes.co.kr/news/photo/202106/96480_77112_1213.jpg"
+          likeCount={123}
+          title="파라솔을 든 여인 - 모네부인과 그녀의 아들"
+          artist="클로드 모네"
+          profileImageUrl="https://upload.wikimedia.org/wikipedia/commons/a/a7/Inwangjesaekdo.jpg"
+          nickname="미니마니모네"
+          date="2024.3.19."
+          content="모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 모네의 미니마니모네 "
+        />
+      </div>
+    </>
+  );
 }
