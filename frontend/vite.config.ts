09a66330243787a9bc2dff8479612e120aa5d946
KMJ <kmjk1117@gmail.com>
2025-03-26 10:19:50 +0900

    🔧 [FE] chore: 절대경로 설정 추가 (tsconfig, vite.config 수정)
     @types/node 설치 및 🔧 절대경로 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+    }
+  },
 });
