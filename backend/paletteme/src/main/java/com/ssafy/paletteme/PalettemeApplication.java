577a1df420b0d61dd94119d8318ddb90a42af9e4
노영단 <shdbwjd7052@naver.com>
2025-03-27 14:40:20 +0900

    ✨ [BE] feat: MySQL 데이터 갱신 시 Logstash를 통해 Elasticsearch 내의 인덱스도 갱신될 수 있도록 파이프라인 구축 (S12P21A505-160)
    MySQL 데이터 갱신 시 Elasticsearch의 데이터도 갱신될 수 있도록 스케줄링 코드 추가

--- backend/paletteme/src/main/java/com/ssafy/paletteme/PalettemeApplication.java
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/PalettemeApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.paletteme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PalettemeApplication {
 
 	public static void main(String[] args) {
