0127f6d4cb8c7a1a628f53c562aafef511da6086
hyunhee <hh.hyunheekim@gmail.com>
2025-03-27 17:48:18 +0900

    ✨ [FE] feat: 작품 상세페이지 기본 틀 (S12P21A505-138)

--- frontend/src/pages/ArtworkPage.tsx
+++ frontend/src/pages/ArtworkPage.tsx
@@ -1,3 +1,38 @@
+import { ArtworkImage } from "@/shared/components/artworks/ArtworkImage";
+import { ArtworkMeta } from "@/shared/components/artworks/ArtworkMeta";
+import { DescriptionBox } from "@/shared/components/textboxes/DescriptionBox";
+import { WhiteContainer } from "@/shared/components/textboxes/WhiteRoundedContainer";
+import { CommentBox } from "@/shared/components/textboxes/CommentBox";
+import { AIDocentBox } from "@/shared/components/textboxes/AIDocentBox";
+
+import { artworkDummy } from "@/shared/dummy/artworkDummy";
+import { commentDummy } from "@/shared/dummy/commentDummy";
+import { aiDocentResponses } from "@/shared/dummy/aiDocentRes";
+
 export default function ArtworkPage() {
-  return <div>ArtworkPage </div>;
+  return (
+    <div className="bg-neutral-100 min-h-screen">
+      <div className="bg-neutral-200  pt-2">
+        <ArtworkImage artwork={artworkDummy} />
+      </div>
+      <WhiteContainer withTopRound withMarginTop>
+        <ArtworkMeta artwork={artworkDummy} showYear showLocation />
+        <DescriptionBox description={artworkDummy.description} />
+      </WhiteContainer>
+
+      <WhiteContainer className="mt-2">
+        <AIDocentBox
+          onFetchExplanation={() =>
+            new Promise((res) =>
+              setTimeout(() => res(aiDocentResponses["1"]), 1500)
+            )
+          }
+        />
+      </WhiteContainer>
+
+      <WhiteContainer className="mt-2 pb-28">
+        <CommentBox comments={commentDummy} />
+      </WhiteContainer>
+    </div>
+  );
 }
