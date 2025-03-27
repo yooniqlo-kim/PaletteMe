d0b7d63a1e5275a2e33122e639e8db606428c915
김지윤 <jaegal0920@gmail.com>
2025-03-27 11:22:37 +0900

    ✨ [FE] feat: 프로필 페이지 완성 (S12P21A505-135)

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,9 +1,20 @@
-import UserImage from "@/shared/components/user/UserImage";
+import Menu from "@/features/profile/Menu";
+import UserMeta from "@/features/profile/UserMeta";
+import UserProfile from "@/features/profile/UserProfile";
+import Pallette from "@/shared/components/user/Pallette";
 
 export default function ProfilePage() {
   return (
-    <div>
-      <UserImage />
-    </div>
+    <section className="px-3 py-3 pb-[3.75rem] flex flex-col gap-6 box-border">
+      <UserProfile />
+      <UserMeta review={0} like={0} loggedIn={0} />
+      <Pallette level="주황" />
+      <Menu />
+      <div className="text-inactive font-[0.75rem] flex justify-center gap-1 items-center">
+        <a href="/">로그아웃</a>
+        <span>|</span>
+        <a href="/">회원 탈퇴</a>
+      </div>
+    </section>
   );
 }
