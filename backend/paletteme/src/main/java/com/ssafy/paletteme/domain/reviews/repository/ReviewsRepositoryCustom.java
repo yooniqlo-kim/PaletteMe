b7b52401e379424e57edf8e44a4dfab0a9f5d22b
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-03-28 16:49:54 +0900

    ✨ [BE] feat: 리뷰 Entity 생성 (S12P21A505-161)

---
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/domain/reviews/repository/ReviewsRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.ssafy.paletteme.domain.reviews.repository;
+
+import com.ssafy.paletteme.domain.reviews.dto.ReviewSummaryResponse;
+import com.ssafy.paletteme.domain.reviews.dto.ReviewsWithArtworkResponses;
+
+import java.util.List;
+
+public interface ReviewsRepositoryCustom {
+    ReviewsWithArtworkResponses getReviewsWithArtworkResponses(int reviewId);
+    List<ReviewSummaryResponse> findReviewsWithPaging(String artworkId, Integer cursor, int size);
+}
