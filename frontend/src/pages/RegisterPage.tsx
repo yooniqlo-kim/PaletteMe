50a13ba3972b4076bdc2ea09c93bfaca05a0c579
KMJ <kmjk1117@gmail.com>
2025-03-28 13:32:37 +0900

    ♻️ [FE] refactor: 페이지 구조 리팩토링 (S12P21A505-151)

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,12 +1,10 @@
 import RegisterInfoForm from "@/features/register/RegisterInfoForm";
-import Container from "@/shared/components/layout/Container";
 
 export default function RegisterPage() {
   return (
-    <Container>
       <div className="flex p-10">
         <RegisterInfoForm />
       </div>
-    </Container>
+
   );
 }
--- frontend/src/shared/components/layout/RootLayout.tsx
+++ frontend/src/shared/components/layout/RootLayout.tsx
@@ -1,19 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Container from "@/shared/components/layout/Container";
 import Header from "@/shared/components/header/Header";
 import NavBar from "@shared/components/navbar/NavBar";
 import ScrollToTop from "@/shared/components/scroll/ScrollToTop";
 
 export default function RootLayout() {
+  const location = useLocation();
+
+  const hiddenLayoutPaths = ["/login", "/signup", "/wrapped"];
+  const shouldHideLayout = hiddenLayoutPaths.includes(location.pathname);
+
   return (
     <div className="relative h-screen w-screen bg-red-200 flex justify-center items-center">
-      <Header />
-      <Container>
+      {!shouldHideLayout && <Header />}
+      <Container id="scrollable-container">
         <ScrollToTop />
         <Outlet />
       </Container>
-      <NavBar />
+      {!shouldHideLayout && <NavBar />}
     </div>
   );
 }
-
