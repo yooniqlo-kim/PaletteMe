170186a439f50fe3943606fa64058f83fd7a133d
김지윤 <jaegal0920@gmail.com>
2025-03-29 20:51:49 +0900

    ♻️ [FE] refactor: default image 변경 (S12P21A505-148)

--- frontend/src/shared/components/user/UserImage.tsx
+++ frontend/src/shared/components/user/UserImage.tsx
@@ -1,11 +1,11 @@
-import defaultImg from "../../../assets/images/profile.jpg";
+import defaultImg from "@/assets/images/MainLogo.png";
 
 export default function UserImage({ userImg = defaultImg }) {
   return (
     <div className="w-[132px] h-[132px] cursor-pointer">
       <img
         src={userImg}
-        alt=""
+        alt="profile"
         className="rounded-[50%] bg-neutral-2 w-full h-full object-cover"
       />
     </div>
