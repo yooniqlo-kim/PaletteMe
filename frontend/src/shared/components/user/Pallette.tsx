67ff05b43a96b82a88746d87f995beeb5b70dd0d
김지윤 <jaegal0920@gmail.com>
2025-03-29 23:23:23 +0900

    ♻️ [FE] refactor: make the component more flexible (S12P21A505-126)

--- frontend/src/shared/components/user/Pallette.tsx
+++ frontend/src/shared/components/user/Pallette.tsx
@@ -1,17 +1,19 @@
 import { color } from "@/shared/utils/color";
 
 type PalletteProps = {
-  level: keyof typeof color; // level의 타입을 color 객체의 key 중 하나로 제한
+  level?: keyof typeof color; // level의 타입을 color 객체의 key 중 하나로 제한
 };
 
 export default function Pallette({ level }: PalletteProps) {
   return (
     <article className="h-60 flex flex-col bg-neutral-1 rounded-ps px-[6.25rem] py-[1.75rem]">
-      <p className="font-semibold text-sm flex justify-center gap-2 items-center">
-        현재 등급은
-        <strong style={{ color: color[level] }}> {level}</strong>
-        입니다.
-      </p>
+      {level && (
+        <p className="font-semibold text-sm flex justify-center gap-2 items-center">
+          현재 등급은
+          <strong style={{ color: color[level] }}> {level}</strong>
+          입니다.
+        </p>
+      )}
     </article>
   );
 }
