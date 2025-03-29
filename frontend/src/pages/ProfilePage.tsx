d8ad4f979082afe04ddc79181e413e49809a4828
김지윤 <jaegal0920@gmail.com>
2025-03-29 23:28:34 +0900

    ✨ [FE] feat: 회원 탈퇴 페이지로 연결할 링크 추가 (S12P21A505-126)

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -2,6 +2,7 @@ import Menu from "@/features/profile/Menu";
 import UserMeta from "@/features/profile/UserMeta";
 import UserProfile from "@/features/profile/UserProfile";
 import Pallette from "@/shared/components/user/Pallette";
+import { Link } from "react-router";
 
 export default function ProfilePage() {
   return (
@@ -13,7 +14,7 @@ export default function ProfilePage() {
       <div className="text-inactive font-[0.75rem] flex justify-center gap-1 items-center">
         <a href="/">로그아웃</a>
         <span>|</span>
-        <a href="/">회원 탈퇴</a>
+        <Link to="delete">회원 탈퇴</Link>
       </div>
     </section>
   );
