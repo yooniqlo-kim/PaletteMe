181b6e201465e9f754ca7a37edabc036e6d955fd
김지윤 <jaegal0920@gmail.com>
2025-03-29 20:56:14 +0900

    ♻️ [FE] refactor: portal과 연결할 div 위치 변경

--- frontend/src/shared/components/layout/Container.tsx
+++ frontend/src/shared/components/layout/Container.tsx
@@ -15,10 +15,9 @@ export default function Container({
     <div
       id={id}
       className={`w-full max-w-[25.75rem] h-full bg-white overflow-y-auto ${
-        hidePadding ? '' : 'pt-[3.5rem] pb-[3.75rem]'
+        hidePadding ? "" : "pt-[3.5rem] pb-[3.75rem]"
       }`}
     >
-      <div id="modal"></div>
       {children}
     </div>
   );
