4faf555ecb826c9ee65cc40cb5f185748fba162b
KMJ <kmjk1117@gmail.com>
2025-03-31 13:06:03 +0900

    ♻️ [FE] refactor: 작품 카드 수정 (S12P21A505-194)
    회원가입 시, 작품카드 클릭하면 좋아요 클릭되도록 수정

--- frontend/src/features/register/RegisterInfoForm.tsx
+++ frontend/src/features/register/RegisterInfoForm.tsx
@@ -1,7 +1,7 @@
-import Button from "@/shared/components/Buttons/Button";
-import Input from "@/shared/components/Form/Input";
-import InputContainer from "@/shared/components/Form/InputContainer";
-import Label from "@/shared/components/Form/Label";
+import Button from "@/shared/components/buttons/Button";
+import Input from "@/shared/components/form/Input";
+import InputContainer from "@/shared/components/form/InputContainer";
+import Label from "@/shared/components/form/Label";
 import { FormEvent } from "react";
 
 type RegisterInfoFormProps = {
