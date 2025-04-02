ed7863331320ae49e42a95ffaa94aa7be54e9f20
김지윤 <jaegal0920@gmail.com>
2025-04-02 17:35:27 +0900

    ✨ [FE] feat: react hook form 적용 (S12P21A505-181)

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,27 +1,45 @@
 import { useAuth } from "@/features/auth/useAuth";
 import Button from "@/shared/components/buttons/Button";
-import Form from "@/shared/components/form/Form";
 import Input from "@/shared/components/form/Input";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+
+type LoginFormValues = {
+  id: string;
+  password: string;
+};
 
 export default function LoginPage() {
   const { handleLogin } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<LoginFormValues>({ mode: "onChange" });
 
   return (
     <div className="flex justify-center items-center h-full">
-      <Form
-        onSave={(data) => handleLogin(data as { id: string; password: string })}
+      <form
+        onSubmit={handleSubmit(handleLogin)}
         className="w-70 flex flex-col justify-center items-center gap-16">
         <h2 className="font-extrabold text-lg text-primary">PaletteMe</h2>
         <div className="w-full flex flex-col gap-10">
-          <Input id="id" name="id" type="text" placeholder="아이디" />
           <Input
+            {...register("id", { required: "아이디는 필수값입니다." })}
+            id="id"
+            type="text"
+            placeholder="아이디"
+            fallback={errors.id && errors.id.message}
+          />
+          <Input
+            {...register("password", { required: "비밀번호는 필수값입니다." })}
             id="password"
-            name="password"
             type="password"
             placeholder="비밀번호"
+            fallback={errors.password && errors.password.message}
           />
-          <Button size="L" type="submit">
+
+          <Button size="L" type="submit" disabled={isSubmitting || !isValid}>
             로그인
           </Button>
         </div>
@@ -34,7 +52,7 @@ export default function LoginPage() {
             홈으로 가기
           </Link>
         </div>
-      </Form>
+      </form>
     </div>
   );
 }
