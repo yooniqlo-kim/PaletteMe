6a9e2f66f38d371e3fd8c1daa80b4133b6c923ca
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-03 14:54:12 +0900

    ♻️ [BE] chore: 리뷰 api 수정 (S12P21A505-217)

--- backend/paletteme/src/main/java/com/ssafy/paletteme/domain/reviews/controller/ReviewsController.java
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/domain/reviews/controller/ReviewsController.java
@@ -18,18 +18,19 @@ public class ReviewsController {
     private final ReviewsService reviewService;
     @Operation(summary = "작품 리뷰들 조회 (커서 기반)", description = "처음에는 cursor의 값을 0 혹은 null로 보내주고, 이후로 cursor는 server가 보내준 마지막 reviewId 값을 보내줘야합니다.")
     @GetMapping
-    public ApiResponse<ReviewListResponse> getReviewsCursorPaging( @Parameter(description = "작품 ID", example = "MonaLisa_Leonardo") @RequestParam String artworkId,
-                                                                   @Parameter(description = "마지막 reviewId", example = "13") @RequestParam(required = false) Integer cursor,
-                                                                   @Parameter(description = "한 페이지에 가져올 리뷰 수", example = "10") @RequestParam(defaultValue = "10") int size){
-        ReviewListResponse response = reviewService.getReviewsCursorPaging(artworkId, cursor, size);
+    public ApiResponse<ReviewListResponse> getReviewsCursorPaging(@Parameter(hidden = true) @UserId int userId,
+                                                                  @Parameter(description = "작품 ID", example = "MonaLisa_Leonardo") @RequestParam String artworkId,
+                                                                  @Parameter(description = "마지막 reviewId", example = "13") @RequestParam(required = false) Integer cursor,
+                                                                  @Parameter(description = "한 페이지에 가져올 리뷰 수", example = "10") @RequestParam(defaultValue = "10") int size){
+        ReviewListResponse response = reviewService.getReviewsCursorPaging(userId, artworkId, cursor, size);
         return ApiResponse.success(response);
     }
 
     @Operation(summary = "리뷰 작성", description = "로그인한 사용자만 리뷰 작성가능")
     @PostMapping("/write")
-    public ApiResponse<Void> writeReview(@Parameter(hidden = true) @UserId int userId, @RequestBody ReviewsWriteRequest request) {
-        reviewService.writeReview(userId, request);
-        return ApiResponse.success();
+    public ApiResponse<ReviewWriteResponse> writeReview(@Parameter(hidden = true) @UserId int userId, @RequestBody ReviewsWriteRequest request) {
+        ReviewWriteResponse response = reviewService.writeReview(userId, request);
+        return ApiResponse.success(response);
     }
 
     @Operation(summary = "리뷰 상세 조회", description = "리뷰 ID를 기준으로 리뷰와 작품 상제 정보도 조회")
