21bff1a11ae0573a43cd030f7eaf037716155d41
김지윤 <jaegal0920@gmail.com>
2025-04-06 16:02:00 +0900

    ✨ [FE] feat: 회원 정보 페이지 등급 이미지 추가

--- frontend/src/features/profile/UserProfile.tsx
+++ frontend/src/features/profile/UserProfile.tsx
@@ -3,17 +3,22 @@ import IconCamera from "@/shared/components/icons/IconCamera";
 import UserImage from "@/shared/components/user/UserImage";
 import { useNavigate } from "react-router";
 
-export default function UserProfile() {
+type UserProfileProps = {
+  nickname: string;
+  image?: string;
+};
+
+export default function UserProfile({ nickname, image }: UserProfileProps) {
   const navigate = useNavigate();
 
   return (
     <article className="flex justify-between items-center">
       <span>
-        <h2 className="text-lg font-bold">모네덕후</h2>
+        <h2 className="text-lg font-bold">{nickname}</h2>
         <p className="text-sm font-medium">어떤 작품을 디깅하고 계신가요?</p>
       </span>
       <span className="relative" onClick={() => navigate("update-profile")}>
-        <UserImage />
+        <UserImage userImg={image} />
         <span className="absolute bottom-0">
           <RoundedButton identifier="user">
             <IconCamera />
