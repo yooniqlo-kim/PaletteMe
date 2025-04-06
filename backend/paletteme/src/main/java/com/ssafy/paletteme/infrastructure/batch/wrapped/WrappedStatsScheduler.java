893ded0d842ccc62c915a33c35b0805061bedde6
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-06 14:09:27 +0900

    ♻️ [BE] refact: 주입 받을 빈 명시(S12P21A505-225)

--- backend/paletteme/src/main/java/com/ssafy/paletteme/infrastructure/batch/wrapped/WrappedStatsScheduler.java
+++ backend/paletteme/src/main/java/com/ssafy/paletteme/infrastructure/batch/wrapped/WrappedStatsScheduler.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,14 +18,23 @@ import java.time.LocalTime;
 import java.time.temporal.TemporalAdjusters;
 
 @Component
-@RequiredArgsConstructor
 public class WrappedStatsScheduler {
     private final JobLauncher jobLauncher;
     private final Job wrappedStatsJob;
     private final WrappedRepository wrappedRepository;
+
     private static final Logger logger = LoggerFactory.getLogger(WrappedStatsScheduler.class);
 
-    //@Scheduled(cron = "*/10 * * * * *") // 15초마다 실행
+    // 추후에 Job을 여러 개 만들 수도 있으니, 주입 받을 빈 직접 명시하기
+    public WrappedStatsScheduler(JobLauncher jobLauncher,
+                                 @Qualifier("wrappedStatsJob") Job wrappedStatsJob,
+                                 WrappedRepository wrappedRepository) {
+        this.jobLauncher = jobLauncher;
+        this.wrappedStatsJob = wrappedStatsJob;
+        this.wrappedRepository = wrappedRepository;
+    }
+
+    // @Scheduled(cron = "*/10 * * * * *") // 15초마다 실행
     @Scheduled(cron = "0 0 0 1 * *") // 매달 1일 정각에 실행
     public void runWrappedStatsJob() {
         wrappedRepository.deleteAllInBatch();
@@ -44,7 +54,7 @@ public class WrappedStatsScheduler {
             jobLauncher.run(wrappedStatsJob, params);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("wrappedStats 스케줄러 실행 중 오류 발생", e);
         }
     }
 }
