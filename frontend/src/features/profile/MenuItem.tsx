7880439c84808a639d1c8fdd2db7f3189e82f385
김지윤 <jaegal0920@gmail.com>
2025-04-07 12:22:50 +0900

    💄 [FE] style: 회원 프로필 페이지 스타일 변경

--- frontend/src/features/profile/MenuItem.tsx
+++ frontend/src/features/profile/MenuItem.tsx
@@ -9,7 +9,7 @@ export default function MenuItem({ name, onClick }: MenuItemProps) {
   return (
     <li
       onClick={onClick}
-      className="flex justify-between px-2 py-4 box-border items-center rounded-ps border border-neutral-5 text-neutral-7 cursor-pointer">
+      className="flex justify-between px-2 py-4 box-border items-center rounded-ps border border-neutral-3 text-neutral-7 cursor-pointer">
       <p>{name}</p>
       <IconSmallRigthArrow />
     </li>
