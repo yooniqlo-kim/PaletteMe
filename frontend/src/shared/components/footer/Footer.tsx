dc43aa93571b167993efcc7cbd8a5413c87808af
KMJ <kmjk1117@gmail.com>
2025-04-10 14:15:57 +0900

    ♻️ [FE] refactor: footer 깃헙 링크 추가 (S12P21A505-248)

--- frontend/src/shared/components/footer/Footer.tsx
+++ frontend/src/shared/components/footer/Footer.tsx
@@ -5,8 +5,17 @@ export default function Footer() {
         <p className="mb-1">이미지 출처는 퍼블릭 도메인입니다.</p>
   
         <div className="text-[10px] text-neutral-5 p-2">
-          <p className="mb-1">팀원 : 김윤 · 강명주 · 김지윤 · 김현희 · 노영단 · 이학준</p>
-          <p className="mb-1">디자인 : <a href="https://instagram.com/salgu_jungle" target="_blank" rel="noopener noreferrer" className="underline">@salgu_jungle</a></p>
+          <p className="mb-1">
+            팀원:{" "}
+            <a href="https://github.com/yooniqlo-kim" target="_blank" rel="noopener noreferrer">김윤</a> ·{" "}
+            <a href="https://github.com/notrealsilk" target="_blank" rel="noopener noreferrer">강명주</a> ·{" "}
+            <a href="https://github.com/hyuun13" target="_blank" rel="noopener noreferrer">김지윤</a> ·{" "}
+            <a href="https://github.com/hyunhee-dev" target="_blank" rel="noopener noreferrer">김현희</a> ·{" "}
+            <a href="https://github.com/YoungdanNoh" target="_blank" rel="noopener noreferrer">노영단</a> ·{" "}
+            <a href="https://github.com/lhj4499" target="_blank" rel="noopener noreferrer">이학준</a>
+          </p>
+          {/* <p className="mb-1">디자인 : <a href="https://instagram.com/salgu_jungle" target="_blank" rel="noopener noreferrer">@salgu_jungle</a></p> */}
+          <p className="mb-1">디자인 : <a target="_blank" rel="noopener noreferrer">@salgu_jungle</a></p>
         </div>
       </footer>
     );
